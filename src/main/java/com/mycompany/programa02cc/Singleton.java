aff7053846b810dbe38caeb209afefab31050e21
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-05-02 23:18:40 -0600

    sonar

---
+++ src/main/java/com/mycompany/programa02cc/Singleton.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programa02cc;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author irvin
+ */
+public class Singleton {
+    
+    private static Singleton singleton = null;
+    
+    public static Singleton getInstance() {
+        if (singleton == null)
+            singleton = new Singleton();
+        
+        return singleton;
+    }
+    
+    private Singleton () {
+        
+    }
+    
+    public void imprimir() {
+         Logger.getLogger(Singleton.class.getName()).log(Level.INFO, "Hola Mundo");
+    }
+    
+}
